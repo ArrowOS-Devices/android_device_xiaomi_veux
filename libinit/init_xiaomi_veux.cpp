aa823b223980de1164f3fd7b7af2cab1cd8a94b3
kleidione Freitas <kleidione@gmail.com>
2022-03-24 16:18:27 -0300

    veux: proprietary-files: Update to S vendor blobs
    - Extracted veux-user 12 SKQ1.211006.001 V13.0.2.0.SKCMIXM

--- libinit/init_xiaomi_veux.cpp
+++ libinit/init_xiaomi_veux.cpp
@@ -18,7 +18,7 @@ static const variant_info_t veux_global_info = {
     .marketname = "Redmi Note 11 Pro 5G",
     .model = "2201116SG",
     .name = "veux_global",
-    .build_fingerprint = "Redmi/veux_global/veux:11/RKQ1.211001.001/V13.0.13.0.RKCMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/veux_global/veux:12/SKQ1.211006.001/V13.0.2.0.SKCMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -32,7 +32,7 @@ static const variant_info_t veux_info = {
     .marketname = "Redmi Note 11E Pro",
     .model = "2201116SC",
     .name = "veux",
-    .build_fingerprint = "Redmi/veux/veux:11/RKQ1.211001.001/V13.0.13.0.RKCMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/veux/veux:12/SKQ1.211006.001/V13.0.2.0.SKCMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -46,7 +46,7 @@ static const variant_info_t peux_in_info = {
     .marketname = "Redmi Note 11 Pro+ 5G",
     .model = "2201116SI",
     .name = "peux_in",
-    .build_fingerprint = "Redmi/peux_in/peux:11/RKQ1.211001.001/V13.0.13.0.RKCMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/peux_in/peux:12/SKQ1.211006.001/V13.0.2.0.SKCMIXM:user/release-keys",
 };
 
 static const variant_info_t veux_jp_info = {
@@ -58,7 +58,7 @@ static const variant_info_t veux_jp_info = {
     .marketname = "Redmi Note 11 Pro 5G",
     .model = "2201116SR",
     .name = "veux_jp",
-    .build_fingerprint = "Redmi/veux_jp/veux:11/RKQ1.211001.001/V13.0.13.0.RKCMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/veux_jp/veux:12/SKQ1.211006.001/V13.0.2.0.SKCMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -72,7 +72,7 @@ static const variant_info_t veux_p_global_info = {
     .marketname = "POCO X4 Pro 5G",
     .model = "2201116PG",
     .name = "veux_p_global",
-    .build_fingerprint = "POCO/veux_p_global/veux:11/RKQ1.211001.001/V13.0.13.0.RKCMIXM:user/release-keys",
+    .build_fingerprint = "POCO/veux_p_global/veux:12/SKQ1.211006.001/V13.0.2.0.SKCMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -86,7 +86,7 @@ static const variant_info_t peux_p_in_info = {
     .marketname = "POCO X4 Pro 5G",
     .model = "2201116PI",
     .name = "peux_p_in",
-    .build_fingerprint = "POCO/peux_p_in/peux:11/RKQ1.211001.001/V13.0.13.0.RKCMIXM:user/release-keys",
+    .build_fingerprint = "POCO/peux_p_in/peux:12/SKQ1.211006.001/V13.0.2.0.SKCMIXM:user/release-keys",
 };
 
 static const std::vector<variant_info_t> variants = {
